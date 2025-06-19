12cba957dc5df1934d685c9e768b2eeea2868be1
Nimra Yaqoob <nimrayaqoob816@gmail.com>
2025-06-19 10:45:34 +0500

    Update scanning functionality

--- lib/UI/Screens/RecognizerScreen.dart
+++ lib/UI/Screens/RecognizerScreen.dart
@@ -20,14 +20,20 @@ class _Recognizerscreen extends State<Recognizerscreen> {
     doTextRecognization();
   }
 
+  String result = "";
   doTextRecognization() async {
     InputImage inputImage = InputImage.fromFile(widget.image);
     final RecognizedText recognizedText = await textRecognizer.processImage(
       inputImage,
     );
 
+    result = recognizedText.text;
+
     String text = recognizedText.text;
-    print(text);
+    print(result);
+    setState(() {
+      result;
+    });
     for (TextBlock block in recognizedText.blocks) {
       final Rect rect = block.boundingBox;
       final List<Point<int>> cornerPoints = block.cornerPoints;
@@ -50,7 +56,40 @@ class _Recognizerscreen extends State<Recognizerscreen> {
         backgroundColor: Colors.blueAccent,
         title: Text('Recognizer'),
       ),
-      body: Container(child: Image.file(widget.image)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(child: Image.file(widget.image)),
+            Card(
+              margin: EdgeInsets.all(10),
+              color: Colors.grey.shade300,
+              child: Column(
+                children: [
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.document_scanner, color: Colors.white),
+                          Text(
+                            'Results',
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                          Icon(Icons.copy, color: Colors.white),
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      ),
+                    ),
+                    color: Colors.blueAccent,
+                  ),
+
+                  Text(result,style: TextStyle(fontSize: 18),),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
